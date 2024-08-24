df22b8202d4630e99d2c3a6ef9e942de00b18e42
Anand Sahni <anandsahni57@gmail.com>
2024-08-24 14:30:31 +0530

    test 1

--- popup.js
+++ popup.js
@@ -29,6 +29,11 @@ btn.addEventListener('click', async () => {
     );
 });
 
+let num1 = 10;
+let num2 = 10;
+let sum = num1 ++ num2;
+console.log("Sum :", sum);
+
 async function pickColor() {
     try {
         // Picker
